ae110951b757f0316085fcdda2b18bd5b370dd5d
Luuk Verboeket <Lverboeket@hotmail.com>
2022-04-02 18:35:19 +0200

    Footer + fixes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,63 @@
 import React from "react";
 import styles from "./Footer.module.scss";
+import clsx from "clsx";
+import Link from "next/link";
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p className={styles.paragraph}>[voeten]</p>
+      <div className={styles.container}>
+        <div className={clsx(styles.sub, styles.nav)}>
+          <h2>Menu</h2>
+          <ul className={styles.list}>
+            <Link href="/" passHref>
+              <li>Home</li>
+            </Link>
+            <Link href="/#about" passHref>
+              <li>About</li>
+            </Link>
+            <Link href="/#projects" passHref>
+              <li>Projects</li>
+            </Link>
+            <Link href="/#skills" passHref>
+              <li>Skills</li>
+            </Link>
+          </ul>
+        </div>
+        <div className={clsx(styles.sub, styles.contact)}>
+          <h2>contact</h2>
+          <ul className={styles.list}>
+            <li>hey@luukverboeket.nl</li>
+            <li>+31 6 36208954</li>
+          </ul>
+        </div>
+      </div>
+      <div className={styles.bot}>
+        <div className={styles.socials}>
+          <Link
+            href="https://www.linkedin.com/in/luuk-verboeket-6a88021b5/"
+            passHref
+          >
+            <a target="_blank">
+              <img src="/images/icons/iconLinkedin.svg" />
+            </a>
+          </Link>
+          <Link href="https://github.com/Elempiar" passHref>
+            <a target="_blank">
+              <img src="/images/icons/iconGithub.svg" />
+            </a>
+          </Link>
+          <Link
+            href="https://open.spotify.com/playlist/73YkvrxRZ23xZ8LnhcbZCr"
+            passHref
+          >
+            <a target="_blank">
+              <img src="/images/icons/iconSpotify.svg" />
+            </a>
+          </Link>
+        </div>
+        <span className={styles.copyright}>© 2022 LUUK VERBOEKET</span>
+      </div>
     </footer>
   );
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
-          href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,700&display=swap"
           rel="stylesheet"
         />
         <link
